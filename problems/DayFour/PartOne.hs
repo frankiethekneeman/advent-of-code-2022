3b0cda3748020f2da0c948dc6206cae794db328d
Francis Sirizzotti <francis@sirizzotti.info>
2022-12-05 18:09:30 -0500

    Part 4

--- problems/DayFour/PartOne.hs
+++ problems/DayFour/PartOne.hs
@@ -1,12 +1,37 @@
 module DayFour.PartOne where
 
 import Lib.Solution
-import Helpers.Solution
+import Helpers.Input
+import Helpers.Parsing
 
 type Out = Int
 
 examples :: [(String, Out)]
-examples = []
+examples = [("1", 2)]
+
+data Range = Range Integer Integer deriving Eq
+
+instance Grokkable (Range, Range) where 
+    fromResult = grok4 toRanges
+        where toRanges lmin lmax rmin rmax = (Range lmin lmax, Range rmin rmax)
+
+scanRangePair :: Scanner
+scanRangePair = scanRange ^& "," ^& scanRange
+    where scanRange = scanInt ^& "-" ^& scanInt
+
+parseRangePair :: String -> Either String (Range, Range)
+parseRangePair = parse scanRangePair
+
+superRange :: Range -> Range -> Range
+superRange (Range lmin lmax) (Range rmin rmax) = Range (min lmin rmin) (max lmax rmax)
+
+haveCompleteOverlap :: Range -> Range -> Bool
+haveCompleteOverlap l r = l == super || r == super
+    where super = superRange l r
+
+countCompleteOverlaps :: [(Range, Range)] -> Int
+countCompleteOverlaps = length . filter (uncurry haveCompleteOverlap)
+
 
 solution:: AdventProblem Out
-solution = adventOfCode examples (nyi "Parsing" :: String -> Either String Out) (nyi "Solution")
+solution = adventOfCode examples (lineByLineM parseRangePair) (Right . countCompleteOverlaps)
--- problems/DayFour/PartTwo.hs
+++ problems/DayFour/PartTwo.hs
@@ -1,12 +1,40 @@
 module DayFour.PartTwo where
 
 import Lib.Solution
-import Helpers.Solution
+import Helpers.Input
+import Helpers.Parsing
 
 type Out = Int
 
 examples :: [(String, Out)]
-examples = []
+examples = [("1", 4)]
+
+data Range = Range Integer Integer deriving Eq
+
+instance Grokkable (Range, Range) where 
+    fromResult = grok4 toRanges
+        where toRanges lmin lmax rmin rmax = (Range lmin lmax, Range rmin rmax)
+
+scanRangePair :: Scanner
+scanRangePair = scanRange ^& "," ^& scanRange
+    where scanRange = scanInt ^& "-" ^& scanInt
+
+parseRangePair :: String -> Either String (Range, Range)
+parseRangePair = parse scanRangePair
+
+superRange :: Range -> Range -> Range
+superRange (Range lmin lmax) (Range rmin rmax) = Range (min lmin rmin) (max lmax rmax)
+
+size :: Range -> Integer
+size (Range l r) = r - l + 1
+
+haveOverlap :: Range -> Range -> Bool
+haveOverlap l r = size l + size r > superSize
+    where superSize = size $ superRange l r
+
+countOverlaps :: [(Range, Range)] -> Int
+countOverlaps = length . filter (uncurry haveOverlap)
+
 
 solution:: AdventProblem Out
-solution = adventOfCode examples (nyi "Parsing" :: String -> Either String Out) (nyi "Solution")
+solution = adventOfCode examples (lineByLineM parseRangePair) (Right . countOverlaps)
