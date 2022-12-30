f8d80a4fa04e0e3328fbf50ad704899bff0779ba
Francis Sirizzotti <francis@sirizzotti.info>
2022-12-29 20:17:01 -0500

    Day 16, working

--- problems/DaySixteen/PartTwo.hs
+++ problems/DaySixteen/PartTwo.hs
@@ -9,95 +9,107 @@ import Lib.Types
 import Helpers.Input
 import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
-import DaySixteen.PartOne(Valve(..), parseValve, buildWorkingValveMap, closedFlows)
+import DaySixteen.PartOne(Valve(..), parseValve)
 import GHC.Utils.Misc(liftFst)
 import Data.Maybe(catMaybes)
 import Data.Either.Combinators(maybeToRight)
-import Data.List(sort)
+import Data.List(sort, intercalate)
 import qualified Algorithms.AStar as AStar
 
 -- | The type of the answer to this problem
 type Out = Int
 
 examples :: [(String, Out)]
-examples = [("1", 1707), ("2", 312), ("3", 92), ("4", 88), ("5", 264), ("6", 2528)]
+examples = [("1", 1707)
+           , ("2", 312)
+           , ("3", 92)
+           , ("4", 88)
+           , ("5", 264)
+           , ("6", 554)
+           , ("friend", 2528)
+           ]
 
-data Status = Opening Int String | Start | Stop deriving (Eq, Ord, Show)
+type EdgeMap = Map.Map String [String]
+type WeightedEdgeMap = Map.Map String [(Int, String)]
 
-fromTuple :: (Int, String) -> Status
-fromTuple = uncurry Opening
+buildEdgeMap :: [Valve] -> EdgeMap
+buildEdgeMap = Map.fromListWith (++) . map edges
+    where edges v = (name v, destinations v)
 
-count :: Status -> Int
-count (Opening x _) = x
-count Stop = 260
-count Start = 0
+orF :: (a -> Bool) -> (a -> Bool) -> a -> Bool
+orF l r x = l x || r x
 
-isRedundant :: Set.Set String -> Status -> Bool
-isRedundant s (Opening _ v) = Set.member v s
-isRedundant _ _ = False
+toWeightedGraph :: [Valve] -> EdgeMap -> WeightedEdgeMap
+toWeightedGraph valves edges = Map.mapWithKey getWeights . Map.restrictKeys edges $ valuable
+    where valuable = Set.fromList . ("AA":) . map name . filter (orF hasFlow isJunction) $ valves
+          hasFlow = (>0) . flowRate
+          isJunction = (>2) . length . destinations
+          getWeights source dests = map (tracePath source) dests
+          tracePath s d
+            | Set.member d valuable = (1, d)
+            | otherwise = liftFst succ . tracePath d . head . filter (/=s) . Map.findWithDefault [] d $ edges
 
-type ValveGraph = Map.Map String [(Int, String)]
+data Status = Moving Int String | Opening String | Standing String deriving (Eq, Ord, Show)
+data State = State Int Status Status (Set.Set String) deriving (Eq, Ord, Show)
 
-tick :: ValveGraph -> Status -> (Maybe String, [Status])
-tick graph Start = (Nothing,) . map  (fromTuple . liftFst pred) . Map.findWithDefault [] "AA" $ graph
-tick _ Stop = (Nothing, [Stop])
-tick graph (Opening n t)
-    | n > 0 = (Nothing, [Opening (n-1) t])
-    | otherwise = (Just t,) . (Stop:) . map fromTuple . Map.findWithDefault [] t $ graph
+tick :: WeightedEdgeMap -> Status -> (Maybe String, [Status])
+tick edges status = case status of
+        Standing n -> (Nothing, concat . map (snd . tick edges) . moveFrom $ n)
+        Opening n -> (Just n, moveFrom n)
+        Moving 0 n -> (Nothing, (Opening n):moveFrom n)
+        Moving k n -> (Nothing, [Moving (pred k) n])
+    where moveFrom node = map (uncurry Moving . liftFst pred) . Map.findWithDefault [] node $ edges
 
-data State = State Status Status (Set.Set String) deriving (Eq, Ord, Show)
-    -- { me :: Status
-    -- , elephant :: Status
-    -- , openValves :: Set.Set String
-    -- }
+progress :: WeightedEdgeMap -> State -> [State]
+progress edges (State e me elephant open) = [State (e + 1) me' elephant' open' | me' <- myTicks,
+                                                                                elephant' <- elephantTicks]
+    where open' = Set.union open . Set.fromList . catMaybes $ [myOpen, elephantOpen]
+          (myOpen, myTicks) = tick edges me
+          (elephantOpen, elephantTicks) = tick edges elephant
 
-rectify :: State -> State 
-rectify s@(State left right open)
-    | count right < count left = State right left open
-    | otherwise = s 
-
-
-
-colliding :: Set.Set String -> State -> Bool
-colliding targets (State (Opening _ me) (Opening _ elephant) open) 
-    | 1 == Set.size (Set.difference targets open) = False
-    | otherwise = me == elephant
-colliding _ _ = False
-
-neighbors :: ValveGraph -> State -> [State]
-neighbors graph (State me elephant open) = map rectify potential
-    where potential = [State me' elephant' open' | me' <- filter valuable myTicks,
-                                                   elephant' <- filter valuable elephantTicks]
-          valuable = not . isRedundant open'
-          open' = Set.union open . Set.fromList . catMaybes $ [iOpened, elephantOpened]
-          (iOpened, myTicks) = tick graph me
-          (elephantOpened, elephantTicks) = tick graph elephant
-
-cost :: [Valve] -> State -> Integer
-cost valves (State _ _ open) = sum . closedFlows valves $ open
+--countPathsLessThan :: WeightedEdgeMap -> Int -> String -> Int
+--countPathsLessThan edges n curr
+--        | n < 0 = 0
+--        | n == 0 = 1
+--        | otherwise = sum . map (uncurry recurse) $ dests
+--    where recurse cost d = countPathsLessThan edges (n-cost) d
+--          dests = Map.findWithDefault [] curr $ edges
 
 sumPairs :: Num a => [a] -> [a]
 sumPairs [] = []
 sumPairs [x] = [x]
-sumPairs (first:second:rest) = first + second : sumPairs rest
+sumPairs (x1:x2:xs) = x1 + x2 : sumPairs xs
 
-optimalOpens :: [Valve] -> State -> Integer
-optimalOpens valves (State me elephant open) = wait * currCost + sum (tail steps)
-    where wait = succ . toInteger $ min (count me) (count elephant)
-          steps = scanl (-) currCost rates
-          currCost = sum rates
-          rates = sumPairs . reverse . sort . closedFlows valves $ open
+timeToNextOpen :: Set.Set String -> Int -> Status -> Int
+timeToNextOpen open minWalk status = if v == "AA" || Set.member v open then minWalk + base else base
+    where (base, v) = case status of
+              (Opening n) -> (0, n)
+              (Standing n) -> (0, n)
+              (Moving t n) -> (t, n)
 
-maximizeFlowWithElephant :: [Valve] -> Result Int
-maximizeFlowWithElephant valves = (optimal -) . fst <$> path
-    where optimal = (26*) . fromInteger . sum . map flowRate $ valves
-          path = maybeToRight "No Path Found" $ AStar.weighted heuristic nexts (State Start Start Set.empty)
-          heuristic = fromInteger . optimalOpens valves
-          nexts s = map (AStar.Step stepCost) . filter (not . colliding targets) . neighbors lookups $ s
-            where stepCost = fromInteger $ cost valves s
-          targets = Set.fromList . map snd . Map.findWithDefault [] "AA" $ lookups
-          lookups = buildWorkingValveMap valves
+missedFlow :: [Valve] -> State -> [Integer]
+missedFlow valves (State _ _ _ open) = map flowRate closedValves
+    where closedValves = filter (flip Set.notMember open . name) valves
 
+optimalOpens :: [Valve] -> Int -> State -> Integer
+optimalOpens valves minWalk s@(State e me elephant open) = sum . take (26 - e) $ costs
+    where costs = scanl (-) curr $ (replicate delay 0) ++ flowChanges
+          delay = minimum . map (timeToNextOpen open minWalk) $ [me, elephant]
+          curr = sum closedValves
+          flowChanges = intercalate (replicate minWalk 0) . map (pure) $ steps
+          steps = sumPairs . reverse . sort $ closedValves
+          closedValves = missedFlow valves $ s
+
+nonce :: [Valve] -> Result Out
+nonce v = (optimal - ) . fst <$> path
+    where optimal = (26 *) . fromInteger . sum . map flowRate $ v
+          path = maybeToRight "No Path Found" .  AStar.weighted heuristic nexts $ start
+          start = State 0 (Standing "AA") (Standing "AA") Set.empty
+          heuristic = fromInteger . optimalOpens v minWalk
+          minWalk = minimum . map fst . concat . Map.elems $ weightedEdges
+          nexts s = map (AStar.Step cost) . progress weightedEdges $ s
+            where cost = fromInteger . sum . missedFlow v $ s
+          weightedEdges = toWeightedGraph v . buildEdgeMap $ v 
 -- | Solution for Day Sixteen, Part Two
 solution:: AdventProblem Out
-solution = adventOfCode examples (lineByLineM parseValve) maximizeFlowWithElephant
+solution = adventOfCode examples (lineByLineM parseValve) nonce
